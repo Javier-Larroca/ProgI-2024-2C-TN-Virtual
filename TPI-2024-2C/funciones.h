5c3cad81a6343adb9c18f5b0a7a06e725df85081
Javier Agustin Larroca <javier-larroca@hotmail.com>
2024-11-20 21:28:41 -0300

    Se agrega posible resolucion de tpi

---
+++ TPI-2024-2C/funciones.h
@@ -0,0 +1,25 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+using namespace std;
+
+void comenzarJuego(string nombres[], int puntos[], int cantBonzos[], int cantLanzamientosExitosos[]); //Función donde comienza el juego.
+int contarRepeticionesDeNumeroProhibido(int numeroPrhibido, int dados[]); //Cuenta la cantidad de numeros prohibidos obtenidos en una tirada
+void creditos();
+bool dadosIguales(int dados[]); //Función que nos devolvera true en caso de haber un Bonzo Ve a Dormir.
+void definirGanador(string nombres[], int puntos[], int cantBonzos[], int cantLanzamientosExitosos[]); //Definira el ganador dependiendo de los puntos y cantidad de bonzos obtenidos.
+void definirPrimerJugador(string nombre1, string nombre2, string nombres[]); //Define el jugador que comenzara, ordenandolo de manera correspondiente en el vector de nombres.
+void dibujarDado(int d); //Función que dibuja el valor de un dado recibido por parametro.
+void dibujarDadosEnLinea(int dados[], int cantDados); //Función que dibuja en la misma linea 3 dados según los valores que reciba del vector.
+bool finalizarJuego();
+void guardarPartidaEnRanking(string nombres[], int puntos[], int rankingPuntos[], string rankingNombres[]);
+void jugar(int rankingPuntos[], string rankingNombres[]); //Función llamada desde el main para la opción de jugar.
+int lanzarDados(int dados[], int cantDados); //Carga el vector de dados con nros aleatorios entre 1 y 6.
+void mostrarEstadisticas(string nombres[], int puntos[]); //Muestra las primeras 5 posiciones de los vectores de ranking.
+void mostrarPuntosDeLaPartida(string nombres[], int puntos[], int cantBonzos[], int cantLanzamientosExitosos[]);
+void ordenarRanking(string nombres[], int puntos[]); //Ordena los vectores de ranking
+void recalcularPuntos(int puntos[], int cantBonzos[], int cantLanzamientosExitosos[]); //Función que recalculara los puntos de cada jugador dependiendo de cuantos hitos se hayan cumplidos.
+void solicitarNombres(string &nombre1, string &nombre2); //Solicita los nombres de ambos jugadores.
+int tirarDado(); //Función que devuelve un numero entre el 1 y el 6.
+
+#endif // FUNCIONES_H_INCLUDED
---
+++ TPI-2024-2C/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "funciones.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Spanish"); //Seteo el lenguaje de la consola.
+    srand (time(NULL));
+
+    int opc, rankingPuntos[7]= {};
+    string rankingNombres[7]= {};
+    bool juego = true;
+
+    while(juego)
+    {
+        system("cls");
+        cout << " 88\"\"Yb  dP\"Yb  88b 88 8888P  dP\"Yb" << endl;
+        cout << " 88__dP dP   Yb 88Yb88   dP  dP   Yb" << endl;
+        cout << " 88\"\"Yb Yb   dP 88 Y88  dP   Yb   dP" << endl;
+        cout << " 88oodP  YbodP  88  Y8 d8888  YbodP" << endl;
+
+        cout << "------------------------------------" << endl;
+        cout << "          1 - JUGAR" << endl;
+        cout << "          2 - ESTADISTICAS" << endl;
+        cout << "          3 - CREDITOS" << endl;
+        cout << "------------------------------------" << endl;
+        cout << "          0 - SALIR" << endl;
+        cout<<endl<<"          Opción: ";
+        cin >> opc;
+
+        switch (opc)
+        {
+        case 1:
+            jugar(rankingPuntos, rankingNombres);
+            break;
+        case 2:
+            mostrarEstadisticas(rankingNombres, rankingPuntos);
+            break;
+        case 3:
+            creditos();
+            break;
+        case 0:
+            juego = finalizarJuego();
+            break;
+        default:
+            cout << "Opcion invalida" << endl;
+            break;
+        }
+    }
+    return 0;
+}
